9455e5d09fdd1e8dbb08834802b1df2661247ed6
ABSanthosh <a.b.santhosh02@gmail.com>
2021-05-22 15:07:33 +0530

    Css and html changes

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $('.grocery').click(function(){
+        $(this).toggleClass('fa-times');
+        $('nav').toggleClass('nav-toggle');
+    });
+
+    $('nav ul li a').click(function(){
+        $('.fa-hamburger').removeClass('fa-times');
+        $('nav').removeClass('nav-toggle');
+    });
+
+    $(window).on('scroll load', function(){
+        if($(window).scrollTop() > 10){
+            $('header').addClass('header-active');
+        }else{
+            $('header').removeClass('header-active');
+        }
+    });
+});
